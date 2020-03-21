c578d7ffc6b6a664530859f1f708900be197e24c
dgarciam2001 <danielingar65@gmail.com>
2020-03-20 21:45:33 -0600

    base de datos tablas administracion

--- Modelos E-R/umgsystem db.sql
+++ Modelos E-R/umgsystem db.sql
@@ -9,6 +9,7 @@ Asignaturas varchar(1) not null,
 NombreAlumno varchar (60) not null,
 MatriculaAlumno varchar(60) not null,
 DireccionAlumno varchar(60) not null,
+carnet_alumno varchar(12) not null,
 FechaNacAlumno date not null,
 EdadAlumno varchar(60) not null,
 FechaIngresoAlumno date not null,
@@ -17,7 +18,10 @@ PensumCerrado varchar(1) not null,
 FotostaticaTitulo varchar(1) not null,
 CertificacionNM varchar(1) not null,
 DPI_CertificadoNacimiento varchar(1) not null,
-Fotografia varchar(1) not null
+Fotografia varchar(1) not null,
+foreign key (id_certificado) references
+certificado(id_certificado),
+estado_alumno varchar(1) not null
 )engine=innodb;
 
 create table Asignaturas(
@@ -41,9 +45,94 @@ Direccion_Maestro varchar(60) not null,
 Sueldo_Maestro double(5,2) not null
 )engine=innodb;
 
-create table Administracion(
+Insert Into Maestros values("1","Pedro Morales","M",34,"zona 1","2000");
+Insert Into Maestros values("2","Sara Perez","F",39,"zona 10","2050");
+Insert Into Maestros values("3","Byron Torres","M",44,"zona 2","3000");
+Insert Into Maestros values("4","Milsa Ramirez","F",34,"zona 7","2000");
 
+select *from Maestros;
+
+create table Administracion(
 Id_Maestros VARCHAR(10) NOT NULL,
 foreign key (Codigo_Maestros) References
 Maestros(Codigo_Maestros)
 )engine=innodb;
+
+create table Directores(
+Id_Director varchar(10) primary key
+)engine=innodb;
+
+create table Sedes(
+Id_Sede varchar(10)not null,
+Nombre_Sede varchar(60) not null,
+Direccion_sede varchar(80) not null,
+Numero_sede int not null,
+Numero_Telefinco varchar (30) not null,
+Id_Director varchar(10) not null,
+Numero_Estudiante int not null,
+Numero_Maestros int not null,
+Numero_Carreras int not null,
+Estado_Sede varchar (1) not null
+)engine=InnoDB;
+
+create table Jornadas(
+Id_Jornada varchar(10) primary key
+)engine=innodb;
+
+create table Salones(
+Id_Salon varchar(10) primary key,
+Jornada varchar(10) not null,
+foreign key (Jornada) References
+Jornadas(Id_Jornada),
+Horario varchar(10) not null,
+foreign key (Horario) References
+Horarios(Id_Horario),
+Estado_Salon varchar(1) not null,
+Capacidad_Salon int not null,
+Sede varchar(10) not null,
+foreign key (Sede) References
+Sedes(Id_Sede),
+Asignatura varchar(10) not null,
+foreign key (Asignatura) References
+Asignaturas(Id_Asignatura)
+)engine=innodb;
+
+create table Horarios(
+Id_Horarios varchar(5) not null,
+IDAsignatura varchar(10) not null,
+foreign key (IDAsignatura) references
+Asignaturas(IDAsignatura),
+Id_Maestro varchar(10) not null,
+foreign key (Id_Maestro) references
+Asignaturas(Id_Maestro)
+)engine=innodb;
+
+create table certificados(
+id_certificado varchar(5) primary key,
+foreign key (IDAsignatura) references
+Asignaturas(IDAsignatura), 
+estado_asignatura varchar(1) not null
+
+)engine=innodb;
+
+create table factuarion_pagos(
+id_factura varchar(10) primary key,
+foreign key (IDAlumno) references
+Alumnos(IDAlumno),
+fecha_factura date not null,
+monto_factura double(5,2) not null,
+nombre_factura varchar(30) not null
+
+
+)engine=innodb;
+
+
+
+
+
+
+
+
+
+ 
+
